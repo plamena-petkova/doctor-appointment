fe5099fcb0468f055860a013c49636f97b339b53
Plamena Petkova <pl.petkova@gmail.com>
2025-07-15 12:07:42 +0300

    Remove slug

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,38 +1,13 @@
 import DoctorAppointmentLanding from '@/components/ui/DoctorAppointments'
 import { Metadata } from 'next'
 
-interface Params {
-  params: {
-    slug: string
-  }
-}
-
-export async function generateMetadata({ params }: Params): Promise<Metadata> {
-  const { slug } = params
-
-  if (slug === '' || slug === 'doctor-appointment') {
-    return {
-      title: 'Doctor Appointment',
-      description: 'Book your doctor appointment easily.',
-    }
-  }
-
+export const generateMetadata = async (): Promise<Metadata> => {
   return {
-    title: `Page: ${slug}`,
-    description: `This is the page for ${slug}`,
+    title: 'Doctor Appointment',
+    description: 'Book your doctor appointment easily.',
   }
 }
 
-export default function Page({ params }: { params: { slug?: string[] } }) {
-  const slug = params.slug?.join('/') || ''
-
-  if (slug === '' || slug === 'doctor-appointment') {
-    return <DoctorAppointmentLanding jotformUrl="https://form.jotform.com/jsform/251922865951062" />
-  }
-
-  return (
-    <div>
-      <h1>Page for slug: {slug}</h1>
-    </div>
-  )
+export default function Page() {
+  return <DoctorAppointmentLanding jotformUrl="https://form.jotform.com/jsform/251922865951062" />
 }
